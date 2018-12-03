20159f11a41d98eacebc394cf941fd5964aeaa47
Daniel Brewer <danielbrewer2014@comcast.net>
2018-12-03 11:50:56 -0600

    added gitignore

---
+++ src/scripts/foodapi.js
@@ -0,0 +1,14 @@
+fetch("http://localhost:8088/food")
+    .then(foods => foods.json())
+    .then(parsedFoods => {
+        console.table(parsedFoods)
+    })
+
+//     Create a DOM element in your index.html with a class of foodList.
+// Create a function which returns a string template. The template is the HTML representation for a food item.
+let foodToBeDisplayed = () => {
+let 
+}
+// Create a function that inserts an HTML representation of a food into the DOM
+
+document.querySelector("foodlist");
