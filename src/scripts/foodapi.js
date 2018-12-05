c93a14d7f5b4f7151ad5995cb04b80141df18272
Daniel Brewer <danielbrewer2014@comcast.net>
2018-12-05 17:59:01 -0600

    branch fix

--- src/scripts/foodapi.js
+++ src/scripts/foodapi.js
@@ -1,13 +1,34 @@
 fetch("http://localhost:8088/food")
     .then(foods => foods.json())
     .then(parsedFoods => {
-        console.table(parsedFoods);
+        parsedFoods.forEach(food => {
+            const foodAsHTML = foodFactory(food.name, food.type, food.ethnicity);
+            addFoodToDom(foodAsHTML);
+        });
     });
-
 //     Create a DOM element in your index.html with a class of foodList.
 // Create a function which returns a string template. The template is the HTML representation for a food item.
-let foodToBeDisplayed = () => {
+
+const h2 = (name, style) => {
+    return `<h2 class="${style}">${name}</h2>`;
 };
+const h3 = (name, style) => {
+    return `<h3 class="${style}">${name}</h3>`;
+};
+
+let foodFactory = (name, type, ethnicity) => {
+    return `<div class="fooddiv">
+                ${h2(name, "h2")}
+                ${h3(type, "h3")}
+                ${h3(ethnicity, "h3")}
+            </div>`;
+};
+
 // Create a function that inserts an HTML representation of a food into the DOM
 
-document.querySelector("foodlist");
+
+let addFoodToDom = (stringReceived) => {
+    document.querySelector(".foodlist").innerHTML += stringReceived;
+    };
+
+
